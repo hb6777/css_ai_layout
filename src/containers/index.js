6ec8c750da07aa2ba75cc61c234cfc1ad219e6d5
Renee L <Reneeliew2002@yahoo.com>
2024-05-10 16:34:43 -0400

    css ai layout commit

---
+++ src/containers/index.js
@@ -0,0 +1,6 @@
+export {default as Blog } from './blog/blog'
+export {default as Features } from './features/features'
+export {default as Footer } from './footer/footer'
+export {default as Possibility } from './possibility/possibility'
+export {default as WhatGTP3 } from './whatgtp3/whatgtp3'
+export {default as Header } from './header/header'
